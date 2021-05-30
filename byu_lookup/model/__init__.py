d522d38eb56cdad6e4226c9e4bd44fc3fd8f0143
josh-desktop <rcxwhiz@gmail.com>
2021-05-30 12:07:11 -0600

    Updated the models which are going to be utilizing pandas dataframes for the time being

--- byu_lookup/model/__init__.py
+++ byu_lookup/model/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+
 import pandas as pd
 
 departments_schema = {'name': str,
@@ -18,7 +19,7 @@ buildings_schema = {'name': str}
 section_types_schema = {'section_type': str}
 courses_schema = {'curriculum_id': int,
                   'title_code': int,
-                  'department': str,
+                  'department': int,
                   'catalog_num': int,
                   'catalog_suffix': str,
                   'title': str,
@@ -28,49 +29,50 @@ courses_schema = {'curriculum_id': int,
                   'expired_date': 'datetime64',
                   'effective_yearterm': str,
                   'expired_yearterm': str,
-                  }
-
-
-# this is the only dataframe based object, all the others are plain models
-class Term:
-	def __init__(self,
-	             year: int,
-	             term: int,
-	             timestamp: datetime.datetime):
-		self.year: int = year
-		self.term: int = term
-		self.timestamp: datetime.datetime = timestamp
-
-		self.departments: pd.DataFrame = pd.DataFrame(columns=departments_schema.keys())
-		self.departments.astype(departments_schema)
-
-		self.instructors: pd.DataFrame = pd.DataFrame(columns=instructors_schema.keys())
-		self.instructors.astype(instructors_schema)
-
-		self.instructor_role_types: pd.DataFrame = pd.DataFrame(columns=instructor_role_types_schema.keys())
-		self.instructor_role_types.astype(instructor_role_types_schema)
-
-		self.buildings: pd.DataFrame = pd.DataFrame(columns=buildings_schema.keys())
-		self.buildings.astype(buildings_schema)
-
-		self.section_types: pd.DataFrame = pd.DataFrame(columns=section_types_schema.keys())
-		self.section_types.astype(section_types_schema)
-
-		self.courses: pd.DataFrame = pd.DataFrame()
-		self.credit_types: pd.DataFrame = pd.DataFrame()
-		self.section_modes: pd.DataFrame = pd.DataFrame()
-		self.sections: pd.DataFrame = pd.DataFrame()
-		self.instructor_roles: pd.DataFrame = pd.DataFrame()
-		self.section_times: pd.DataFrame = pd.DataFrame()
-
-	def __eq__(self, other):
-		return isinstance(other, Term) \
-		       and self.year == other.year \
-		       and self.term == other.term \
-		       and self.timestamp == other.timestamp
-
-	def yearterm(self) -> str:
-		return f'{self.year}{self.term}'
+                  'description': str,
+                  'note': str,
+                  'offered': str,
+                  'prerequisite': str,
+                  'recommended': str,
+                  'credit_hours': float,
+                  'lecture_hours': float,
+                  'lab_hours': float,
+                  'honors_approved': str,
+                  'catalog_prereq': str,
+                  'when_taught': str}
+credit_types_schema = {'credit_type': str}
+section_modes_schema = {'section_mode': str,
+                        'description': str}
+sections_schema = {'course': int,
+                   'section_number': int,
+                   'fixed_or_variable': str,
+                   'credit_hours': float,
+                   'min_credit_hours': float,
+                   'is_honors': bool,
+                   'credit_type': int,
+                   'section_type': int,
+                   'start_date': 'datetime64',
+                   'end_date': 'datetime64',
+                   'seats_available': int,
+                   'class_size': int,
+                   'waitlist_size': int,
+                   'mode': int}
+instructor_roles_schema = {'instructor': int,
+                           'section': int,
+                           'role': int}
+section_times_schema = {'section': int,
+                        'begin_time': 'datetime64',
+                        'end_time': 'datetime64',
+                        'building': int,
+                        'room': str,
+                        'sequence_num': int,
+                        'sunday': bool,
+                        'monday': bool,
+                        'tuesday': bool,
+                        'wednesday': bool,
+                        'thursday': bool,
+                        'friday': bool,
+                        'saturday': bool}
 
 
 class Department:
@@ -135,7 +137,7 @@ class SectionType:
 
 class Course:
 	def __init__(self,
-	             term: Term,
+	             index: int,
 	             curriculum_id: int,
 	             title_code: int,
 	             department: Department,
@@ -158,9 +160,8 @@ class Course:
 	             lab_hours: float,
 	             honors_approved: str,
 	             catalog_prereq: str,
-	             when_taught: str,
-	             sections: list):
-		self.term = term
+	             when_taught: str):
+		self.index = index
 		self.curriculum_id = curriculum_id
 		self.title_code = title_code
 		self.department = department
@@ -184,11 +185,9 @@ class Course:
 		self.honors_approved = honors_approved
 		self.catalog_prereq = catalog_prereq
 		self.when_taught = when_taught
-		self.sections = sections
 
 	def __eq__(self, other):
-		return isinstance(other,
-		                  Course) and self.curriculum_id == other.curriculum_id and self.title_code == other.title_code
+		return isinstance(other, Course) and self.curriculum_id == other.curriculum_id and self.title_code == other.title_code
 
 
 class CreditType:
@@ -242,19 +241,17 @@ class Section:
 		self.times = times
 
 	def __eq__(self, other):
-		return isinstance(other,
-		                  Section) and self.course == other.course and self.section_number == other.section_number
+		return isinstance(other, Section) and self.course == other.course and self.section_number == other.section_number
 
 
 class InstructorSectionRole:
-	def __init__(self, instructor: Instructor, section: Section, role: InstructorRole):
+	def __init__(self, instructor: Instructor, section: Section, role: InstructorRoleType):
 		self.instructor = instructor
 		self.section = section
 		self.role = role
 
 	def __eq__(self, other):
-		return isinstance(other,
-		                  InstructorSectionRole) and self.instructor == other.instructor and self.section == other.section and self.role == other.role
+		return isinstance(other, InstructorSectionRole) and self.instructor == other.instructor and self.section == other.section and self.role == other.role
 
 
 class SectionTime:
@@ -287,5 +284,128 @@ class SectionTime:
 		self.saturday = saturday
 
 	def __eq__(self, other):
-		return isinstance(other,
-		                  SectionTime) and self.section == other.section and self.sequence_num == other.sequence_num
+		return isinstance(other, SectionTime) and self.section == other.section and self.sequence_num == other.sequence_num
+
+
+# this is the only dataframe based object, all the others are plain models
+class Term:
+	def __init__(self, year: int,  term: int, timestamp: datetime.datetime):
+		self.year: int = year
+		self.term: int = term
+		self.timestamp: datetime.datetime = timestamp
+
+		self.departments: pd.DataFrame = pd.DataFrame(columns=departments_schema)
+		self.departments = self.departments.astype(departments_schema, copy=False)
+
+		self.instructors: pd.DataFrame = pd.DataFrame(columns=instructors_schema)
+		self.instructors = self.instructors.astype(instructors_schema, copy=False)
+
+		self.instructor_role_types: pd.DataFrame = pd.DataFrame(columns=instructor_role_types_schema)
+		self.instructor_role_types = self.instructor_role_types.astype(instructor_role_types_schema, copy=False)
+
+		self.buildings: pd.DataFrame = pd.DataFrame(columns=buildings_schema)
+		self.buildings = self.buildings.astype(buildings_schema, copy=False)
+
+		self.section_types: pd.DataFrame = pd.DataFrame(columns=section_types_schema)
+		self.section_types = self.section_types.astype(section_types_schema, copy=False)
+
+		self.courses: pd.DataFrame = pd.DataFrame(columns=courses_schema)
+		self.courses = self.courses.astype(courses_schema, copy=False)
+
+		self.credit_types: pd.DataFrame = pd.DataFrame(columns=credit_types_schema)
+		self.credit_types = self.credit_types.astype(credit_types_schema, copy=False)
+
+		self.section_modes: pd.DataFrame = pd.DataFrame(columns=section_modes_schema)
+		self.section_modes = self.section_modes.astype(section_modes_schema, copy=False)
+
+		self.sections: pd.DataFrame = pd.DataFrame(columns=sections_schema)
+		self.sections = self.sections.astype(sections_schema, copy=False)
+
+		self.instructor_roles: pd.DataFrame = pd.DataFrame(columns=instructor_roles_schema)
+		self.instructor_roles = self.instructor_roles.astype(instructor_roles_schema, copy=False)
+
+		self.section_times: pd.DataFrame = pd.DataFrame(columns=section_times_schema)
+		self.section_times = self.section_times.astype(section_times_schema, copy=False)
+
+	def __eq__(self, other):
+		return isinstance(other, Term) \
+		       and self.year == other.year \
+		       and self.term == other.term \
+		       and self.timestamp == other.timestamp
+
+	def yearterm(self) -> str:
+		return f'{self.year}{self.term}'
+
+	def get_courses(self) -> list[Course]:
+		courses = []
+		for index, row in self.courses.iterrows():
+			# TODO need to add some sort of exception catching for this load
+			dept_row = self.departments.iloc[row['department']]
+			department = Department(dept_row['name'], dept_row['abbreviation'])
+			courses.append(Course(index,
+			                      row['curriculum_id'],
+			                      row['title_code'],
+			                      department,
+			                      row['catalog_num'],
+			                      row['catalog_suffix'],
+			                      row['title'],
+			                      row['full_title'],
+			                      row['header_text'],
+			                      row['effective_date'],
+			                      row['expired_date'],
+			                      row['effective_yearterm'],
+			                      row['expired_yearterm'],
+			                      row['description'],
+			                      row['note'],
+			                      row['offered'],
+			                      row['prerequisite'],
+			                      row['recommended'],
+			                      row['credit_hours'],
+			                      row['lecture_hours'],
+			                      row['lab_hours'],
+			                      row['honors_approved'],
+			                      row['catalog_prereq'],
+			                      row['when_taught']))
+		return courses
+
+	def get_sections(self, course: Course) -> list[Section]:
+		sections = []
+		self.sections.set_index(['course'])
+		self.section_times.set_index(['section'])
+		sections_rows = self.sections.loc[[course.index]]
+		for index, row in sections_rows.iterrows():
+			section_times = []
+			sections_rows = self.section_times.loc[[index]]
+			for _, time_row in sections_rows.iterrows():
+				building = Building(self.buildings.iloc[time_row['building']]['name'])
+				section_times.append(SectionTime(index,
+				                                 time_row['begin_time'],
+				                                 time_row['end_time'],
+				                                 building,
+				                                 time_row['room'],
+				                                 time_row['sequence_num'],
+				                                 time_row['sunday'],
+				                                 time_row['monday'],
+				                                 time_row['tuesday'],
+				                                 time_row['wednesday'],
+				                                 time_row['thursday'],
+				                                 time_row['friday'],
+				                                 time_row['saturday']))
+			section_mode_row = self.section_modes.iloc[row['mode']]
+			section_mode = SectionMode(section_mode_row['section_mode'], section_mode_row['description'])
+			sections.append(Section(course,
+			                        row['section_number'],
+			                        row['fixed_or_variable'],
+			                        row['credit_hours'],
+			                        row['min_credit_hours'],
+			                        row['is_honors'],
+			                        row['credit_type'],
+			                        row['section_type'],
+			                        row['start_date'],
+			                        row['end_date'],
+			                        row['seats_available'],
+			                        row['class_size'],
+			                        row['waitlist_size'],
+			                        section_mode,
+			                        section_times))
+		return sections
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import datetime
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import byu_lookup.model
 
 
-# Press the green button in the gutter to run the script.
+def run():
+    term = byu_lookup.model.Term(0, 0, datetime.datetime.now())
+    print(term.sections)
+    print(term.sections.dtypes)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run()
